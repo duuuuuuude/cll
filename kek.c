9fa216e031fd7ee00c5d16be0726a30e04b9bca5
duuuuuuude <pvvner@yandex.ru>
2017-09-23 15:53:59 +0300

    Something going on

--- kek.c
+++ kek.c
@@ -6,7 +6,7 @@ typedef struct node {
     struct node* next;
 } node;
 
-node* create_node(int data) {
+node* create(int data) {
     node* new_node = (node*)malloc(sizeof(node));
 
     new_node->data = data;
@@ -25,13 +25,13 @@ node* prepend(int data, node* prev_head) {
 }
 
 node* append(int data, node* tail) {
-    node* new_node = malloc(sizeof(node));
+    node* new_tail = malloc(sizeof(node));
     
-    tail->next = new_node;
-    new_node->data = data;
-    new_node->next = NULL;
+    tail->next = new_tail;
+    new_tail->data = data;
+    new_tail->next = NULL;
 
-    return new_node;
+    return new_tail;
 } 
 
 void print_list(node* head) {
@@ -41,15 +41,49 @@ void print_list(node* head) {
     }
 }
 
+void menu() {
+    printf("---------------------\n");
+    printf("1. Prepend an item.\n");
+    printf("2. Append an item.\n");
+    printf("3. Show list items.\n");
+    printf("---------------------\n");
+}
+
 int main(int argc, char *argv[]) {
-    node* head = create_node(20);
+    node* head = create(10);
     node *tail = head;
 
-    tail = append(20, tail);
-    tail = append(30, tail);
-    tail = append(40, tail);
+    // tail = append(20, tail);
+    // tail = append(30, tail);
+    // tail = append(40, tail);
     
-    print_list(head);
+    //print_list(head);
+    
+    int command = 0;
+    int data;
+    while(1) {
+        menu();
+        scanf("%d", &command);
+        if (command == -1) 
+            break;
+        switch(command) {
+            case 1:
+                printf("Enter a number to prepend: ");
+                scanf("%d", &data);
+                head = prepend(data, head);
+                break;
+            case 2:
+                printf("Enter a number to append: ");
+                scanf("%d", &data);
+                tail = append(data, tail);
+                break;
+            case 3:
+                print_list(head);
+                break;
+            default:
+                exit(0);
+        }
+    }
 }
 
 
